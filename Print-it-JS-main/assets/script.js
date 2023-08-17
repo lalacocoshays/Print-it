55a94f4d9a3fc5e86d238e69455c29057aef6734
lalacocoshays <132978727+lalacocoshays@users.noreply.github.com>
2023-08-17 15:30:59 +0200

    20230817

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -1,52 +1,53 @@
 const slides = [
 	{
 		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"tagLine":"Impressions tous formats <span> en boutique et en ligne </span>"
 	},
 	{
 		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"tagLine":"Tirages haute définition grand format <span> pour vos bureaux et events</span>"
 	},
 	{
 		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"tagLine":"Grand choix de couleurs <span> de CMJN aux pantones</span>"
 	},
 	{
 		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"tagLine":"Autocollants <span> avec découpe laser sur mesure </span>"
 	}
 ]
 
-const slideshow = document.querySelectorAll(".slides");
-slideshow.forEach((slideshow, indx) => {
-  slideshow.style.transform = `translateX(${indx * 100}%)`;
-});
-
 let curSlide = 0;
-let maxSlide = slideshowlength - 1;
+let maxSlide = slides.length - 1;
 
 const nextSlide = document.querySelector(".arrowright");
 nextSlide.addEventListener("click", function () {
+	let curImage = document.querySelector(".banner-img");
   if (curSlide === maxSlide) {
     curSlide = 0;
   } else {
     curSlide++;
   }
 
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
+	curImage.setAttribute("src","./assets/images/slideshow/"+slides[curSlide].image);
+
+	let curText = document.getElementById("titre");
+	curText.innerHTML= slides[curSlide].tagLine
+
 });
 
 const prevSlide = document.querySelector(".arrowleft");
 prevSlide.addEventListener("click", function () {
+	let curImage = document.querySelector(".banner-img");
   if (curSlide === 0) {
     curSlide = maxSlide;
   } else {
     curSlide--;
   }
 
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
+	curImage.setAttribute("src","./assets/images/slideshow/"+slides[curSlide].image);
+
+	let curText = document.getElementById("titre");
+	curText.innerHTML= slides[curSlide].tagLine
+
 });
